c77d4f5fb334170cc21aa66e951717e4ecf6440d
David Colvin <davidmcolvin@gmail.com>
2020-09-28 22:34:05 -0400

    Make the bubble fire

--- _js/ui.js
+++ _js/ui.js
@@ -14,9 +14,11 @@ BubbleShoot.ui = (function($){
 			return coords;
 		},
 		getBubbleCoords : function(bubble){
+			debugger;
 			var bubbleCoords = bubble.position();
 			bubbleCoords.left += ui.BUBBLE_DIMS / 2;
 			bubbleCoords.top += ui.BUBBLE_DIMS / 2;
+			return bubbleCoords;
 		},
 		getBubbleAngle : function(bubble,e){
 			var mouseCoords = ui.getMouseCoords(e);
@@ -24,11 +26,21 @@ BubbleShoot.ui = (function($){
 			var gameCoords = $("#game").position();
 			var boardLeft = 120;
 			var angle = Math.atan((mouseCoords.x - bubbleCoords.left - boardLeft)
-			  / (bubbleCoords.top + gameCoords.top - mouseCoords.y));
+				/ (bubbleCoords.top + gameCoords.top - mouseCoords.y));
 			if (mouseCoords.y > bubbleCoords.top + gameCoords.top){
 				angle += Math.PI;
 			}
 			return angle;
+		},
+		fireBubble : function(bubble,coords,duration){
+			bubble.getSprite().animate({
+				left:coords.x-ui.BUBBLE_DIMS/2,
+				top:coords.y-ui.BUBBLE_DIMS/2
+			},
+			{
+				duration:duration,
+				easing:"linear"
+			});
 		}
 	};
 	return ui;
